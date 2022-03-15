fedbafa6a36d815b4874cdac2d18b1104269a967
Adrian <amustelier@outlook.com>
2022-03-15 16:10:58 +0100

    Added json data example

---
+++ samples/ServerSide/Infrastructure/DataRawDbDataIndexedDb.cs
@@ -0,0 +1,12 @@
+﻿using DnetIndexedDb;
+using Microsoft.JSInterop;
+
+namespace DnetIndexedDbServer.Infrastructure
+{
+    public class DataRawDbDataIndexedDb : IndexedDbInterop
+    {
+        public DataRawDbDataIndexedDb(IJSRuntime jsRuntime, IndexedDbOptions<DataRawDbDataIndexedDb> options) : base(jsRuntime, options)
+        {
+        }
+    }
+}
